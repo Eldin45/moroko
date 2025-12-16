b3c4cbad32c5039a3a3c9fb8d840d8b8943e06c5
Spicy <nmibrahim19@gmail.com>
2025-12-16 15:15:18 +0100

    src

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "~/css/globals.css";
+import { SEO_CONFIG } from "~/app";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  description: `${SEO_CONFIG.description}`,
+  title: `${SEO_CONFIG.fullName}`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
